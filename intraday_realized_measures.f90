a8089e916310f36844b83d273716731a97310429
Beliavsky <beliavsky@aol.com>
2026-06-29 20:52:27 -0400

    Add daily_log_rv, daily_log_rv_and_rsv_neg, and daily_rv_rsv_and_returns to intraday_realized_measures_mod.

--- intraday_realized_measures.f90
+++ intraday_realized_measures.f90
@@ -24,7 +24,8 @@ module intraday_realized_measures_mod
         real(dp), allocatable :: rq(:)
     end type daily_realized_panel_t
 
-    public :: build_daily_realized_panel, select_realized_measure
+    public :: build_daily_realized_panel, select_realized_measure, daily_log_rv
+    public :: daily_log_rv_and_rsv_neg, daily_rv_rsv_and_returns
 
 contains
 
@@ -118,5 +119,175 @@ contains
         end select
         x = max(x, min_var)
     end subroutine select_realized_measure
+
+    ! Compute daily log realized variance from close-to-close intraday bar returns.
+    ! rv_day = sum of squared log(close_i / close_{i-1}) for consecutive same-day bars.
+    subroutine daily_log_rv(bars, log_rv, dates, ndays)
+        type(ohlcv_series_t), intent(in) :: bars
+        real(dp), allocatable, intent(out) :: log_rv(:)
+        integer,  allocatable, intent(out) :: dates(:)
+        integer,  intent(out) :: ndays
+
+        integer :: i, n, cur_date, prev_date
+        real(dp) :: r, rv_today
+        integer,  allocatable :: tmp_dates(:)
+        real(dp), allocatable :: tmp_rv(:)
+
+        n = bars%nobs()
+        allocate(tmp_dates(n), tmp_rv(n))
+        ndays    = 0
+        prev_date = -1
+        rv_today  = 0.0_dp
+
+        do i = 1, n
+            cur_date = yyyymmdd(bars%timestamp(i)%date)
+            if (cur_date /= prev_date) then
+                if (prev_date > 0 .and. rv_today > 0.0_dp) then
+                    ndays = ndays + 1
+                    tmp_dates(ndays) = prev_date
+                    tmp_rv(ndays)    = log(rv_today)
+                end if
+                rv_today  = 0.0_dp
+                prev_date = cur_date
+            else
+                r        = log(bars%close(i) / bars%close(i-1))
+                rv_today = rv_today + r*r
+            end if
+        end do
+        if (prev_date > 0 .and. rv_today > 0.0_dp) then
+            ndays = ndays + 1
+            tmp_dates(ndays) = prev_date
+            tmp_rv(ndays)    = log(rv_today)
+        end if
+
+        allocate(log_rv(ndays), dates(ndays))
+        log_rv = tmp_rv(1:ndays)
+        dates  = tmp_dates(1:ndays)
+        deallocate(tmp_dates, tmp_rv)
+    end subroutine daily_log_rv
+
+    subroutine daily_log_rv_and_rsv_neg(bars, log_rv, log_rsv_neg, dates, ndays)
+        ! Compute log(RV) and log(RSV^-) from intraday bars in a single pass.
+        ! RSV^- = sum of squared intraday CC returns on down-ticks (negative semi-variance).
+        type(ohlcv_series_t), intent(in)   :: bars
+        real(dp), allocatable, intent(out) :: log_rv(:)       ! daily log realized variance
+        real(dp), allocatable, intent(out) :: log_rsv_neg(:)  ! daily log negative semi-variance
+        integer,  allocatable, intent(out) :: dates(:)
+        integer,              intent(out)  :: ndays
+
+        integer :: i, n, cur_date, prev_date
+        real(dp) :: r, rv_today, rsv_neg_today
+        integer,  allocatable :: tmp_dates(:)
+        real(dp), allocatable :: tmp_rv(:), tmp_neg(:)
+        real(dp), parameter :: min_rsv = 1.0e-12_dp
+
+        n = bars%nobs()
+        allocate(tmp_dates(n), tmp_rv(n), tmp_neg(n))
+        ndays         = 0
+        prev_date     = -1
+        rv_today      = 0.0_dp
+        rsv_neg_today = 0.0_dp
+
+        do i = 1, n
+            cur_date = yyyymmdd(bars%timestamp(i)%date)
+            if (cur_date /= prev_date) then
+                if (prev_date > 0 .and. rv_today > 0.0_dp) then
+                    ndays             = ndays + 1
+                    tmp_dates(ndays)  = prev_date
+                    tmp_rv(ndays)     = log(rv_today)
+                    tmp_neg(ndays)    = log(max(rsv_neg_today, min_rsv))
+                end if
+                rv_today      = 0.0_dp
+                rsv_neg_today = 0.0_dp
+                prev_date     = cur_date
+            else
+                r        = log(bars%close(i) / bars%close(i-1))
+                rv_today = rv_today + r*r
+                if (r < 0.0_dp) rsv_neg_today = rsv_neg_today + r*r
+            end if
+        end do
+        if (prev_date > 0 .and. rv_today > 0.0_dp) then
+            ndays            = ndays + 1
+            tmp_dates(ndays) = prev_date
+            tmp_rv(ndays)    = log(rv_today)
+            tmp_neg(ndays)   = log(max(rsv_neg_today, min_rsv))
+        end if
+
+        allocate(log_rv(ndays), log_rsv_neg(ndays), dates(ndays))
+        log_rv      = tmp_rv(1:ndays)
+        log_rsv_neg = tmp_neg(1:ndays)
+        dates       = tmp_dates(1:ndays)
+        deallocate(tmp_dates, tmp_rv, tmp_neg)
+    end subroutine daily_log_rv_and_rsv_neg
+
+    subroutine daily_rv_rsv_and_returns(bars, log_rv, log_rsv_neg, ret_cc, dates, ndays)
+        ! Compute log(RV), log(RSV^-), and daily close-to-close log-returns in one pass.
+        type(ohlcv_series_t), intent(in)   :: bars
+        real(dp), allocatable, intent(out) :: log_rv(:)       ! daily log realized variance
+        real(dp), allocatable, intent(out) :: log_rsv_neg(:)  ! daily log negative semi-variance
+        real(dp), allocatable, intent(out) :: ret_cc(:)       ! daily close-to-close log-return
+        integer,  allocatable, intent(out) :: dates(:)
+        integer,              intent(out)  :: ndays
+
+        integer :: i, n, cur_date, prev_date
+        real(dp) :: r, rv_today, rsv_neg_today, last_close, prev_day_close
+        integer,  allocatable :: tmp_dates(:)
+        real(dp), allocatable :: tmp_rv(:), tmp_neg(:), tmp_ret(:)
+        real(dp), parameter :: min_rsv = 1.0e-12_dp
+
+        n = bars%nobs()
+        allocate(tmp_dates(n), tmp_rv(n), tmp_neg(n), tmp_ret(n))
+        ndays          = 0
+        prev_date      = -1
+        rv_today       = 0.0_dp
+        rsv_neg_today  = 0.0_dp
+        last_close     = 0.0_dp
+        prev_day_close = 0.0_dp
+
+        do i = 1, n
+            cur_date = yyyymmdd(bars%timestamp(i)%date)
+            if (cur_date /= prev_date) then
+                if (prev_date > 0 .and. rv_today > 0.0_dp) then
+                    ndays             = ndays + 1
+                    tmp_dates(ndays)  = prev_date
+                    tmp_rv(ndays)     = log(rv_today)
+                    tmp_neg(ndays)    = log(max(rsv_neg_today, min_rsv))
+                    ! last_close holds bars%close(i-1) = close of last bar of prev day
+                    if (prev_day_close > 0.0_dp) then
+                        tmp_ret(ndays) = log(last_close / prev_day_close)
+                    else
+                        tmp_ret(ndays) = 0.0_dp
+                    end if
+                    prev_day_close = last_close
+                end if
+                rv_today      = 0.0_dp
+                rsv_neg_today = 0.0_dp
+                prev_date     = cur_date
+            else
+                r        = log(bars%close(i) / bars%close(i-1))
+                rv_today = rv_today + r*r
+                if (r < 0.0_dp) rsv_neg_today = rsv_neg_today + r*r
+            end if
+            last_close = bars%close(i)
+        end do
+        if (prev_date > 0 .and. rv_today > 0.0_dp) then
+            ndays             = ndays + 1
+            tmp_dates(ndays)  = prev_date
+            tmp_rv(ndays)     = log(rv_today)
+            tmp_neg(ndays)    = log(max(rsv_neg_today, min_rsv))
+            if (prev_day_close > 0.0_dp) then
+                tmp_ret(ndays) = log(last_close / prev_day_close)
+            else
+                tmp_ret(ndays) = 0.0_dp
+            end if
+        end if
+
+        allocate(log_rv(ndays), log_rsv_neg(ndays), ret_cc(ndays), dates(ndays))
+        log_rv      = tmp_rv(1:ndays)
+        log_rsv_neg = tmp_neg(1:ndays)
+        ret_cc      = tmp_ret(1:ndays)
+        dates       = tmp_dates(1:ndays)
+        deallocate(tmp_dates, tmp_rv, tmp_neg, tmp_ret)
+    end subroutine daily_rv_rsv_and_returns
 
 end module intraday_realized_measures_mod
